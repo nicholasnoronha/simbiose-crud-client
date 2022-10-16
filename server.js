e3636c3841fc717f8252b4c452cb8c27c061de08
Nicholas Noronha <nicholascaio@hotmail.com>
2022-10-16 18:55:05 -0300

    feat: install express and create server file for heroku deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { resolve } = path;
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/", express.static(resolve(__dirname, "./build")));
+
+app.listen(PORT, (err) => {
+  if (err) return console.log(err);
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
