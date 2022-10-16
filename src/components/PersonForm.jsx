e961bb1ed70b3165197750949d6e36455db542b3
Nicholas Noronha <nicholascaio@hotmail.com>
2022-10-16 14:26:38 -0300

    feat: adding cancel modal to modal

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -109,7 +109,10 @@ const PersonForm = (props) => {
           onChange={changeDataNascimentoHandler}
           value={pessoa.data_nascimento}
         />
+        <div className="new-person_actions-container">
+        <Button onClick={props.onClose} className="cancel-button">Cancelar</Button>
         {handleSubmitButton}
+        </div>
       </form>
     </Modal>
   );
